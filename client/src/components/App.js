d9b3760765b9f9b693dd749f5a11392b9a7c7fea
Yim Jinsung <genuinenessss@gmail.com>
2020-08-16 16:24:05 +0900

    ADD states FIX handleLogin

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,42 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import Router from "./Router";
+import {userApi} from "../api";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLogin: false
-      
+      isLogin: false,
+      userData:{
+        userId: null,
+        email: null,
+        username: null,
+      }
     };
   }
 
-  handleLogin(){
+  handleLogin = async (email, password) => {
+    const userData = await userApi.login(email,password);
+
     this.setState({
-      isLogin: !this.state.isLogin
+      isLogin: !this.state.isLogin,
+      userData: {
+        userId: userData.data.id,
+        email: userData.data.email,
+        usernam: userData.data.username
+      }
     })
-    
   }
 
   render() {
-    const {isLogin} = this.state;
+    const {isLogin, userData} = this.state;
 
     return (
       <>
         <GlobalStyles/>
         <Router 
+          userData = {userData}
           isLogin = {isLogin}
           handleLogin={this.handleLogin.bind(this)}/>
       </>
