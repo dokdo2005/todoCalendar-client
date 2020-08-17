11b4a4dd173b6eb2b98c582a429ef8a6f84c3aa9
spring1826 <dongwon1826@gmail.com>
2020-08-17 20:42:12 +0900

    AddClock

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -4,7 +4,6 @@ import Timer from "react-live-clock";
 
 const Container = styled.div``;
 
-<<<<<<< HEAD
 const Section = styled.div``;
 
 const Clock = () => (
@@ -23,50 +22,3 @@ const Clock = () => (
 );
 
 export default Clock;
-=======
-export class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentTime: null,
-            currentDate: null
-        }
-        this.getTime = null
-        this.getDate = null
-    }
-
-    componentDidMount() {
-        this.getTime = setInterval(() => {
-            this.setState({
-                currentTime: new Date().toLocaleTimeString()
-            })
-        }, 1000);
-        this.getDate = setInterval(() => {
-            this.setState({
-                currentDate: new Date().toLocaleDateString()
-            })
-        }, 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.getTime);
-        clearInterval(this.getDate);
-    }
-
-    render() {
-        if (this.state.currentTime === null || this.state.currentDate === null) {
-            return (
-                <Container>
-                    Loading...
-                </Container>
-            )
-        }
-        return (
-            <Container>
-                <h2>{this.state.currentTime}</h2>
-                <h3>{this.state.currentDate}</h3>
-            </Container>
-        )
-    }
-}
->>>>>>> 002685b39975305a0ffe00bb6bb2057b0947dc50
