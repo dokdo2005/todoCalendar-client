31a4a7470c8c670fd74fcef36624617c8e701028
dokdo2005 <dokdo2005@gmail.com>
2020-08-17 16:50:59 +0900

    add clock component

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -1,12 +1,47 @@
 import React from "react";
-import styled from "styled-components";
 
-const Container = styled.div``;
+export class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTime: null,
+            currentDate: null
+        }
+        this.getTime = null
+        this.getDate = null
+    }
 
-const 
+    componentDidMount() {
+        this.getTime = setInterval(() => {
+            this.setState({
+                currentTime: new Date().toLocaleTimeString()
+            })
+        }, 1000);
+        this.getDate = setInterval(() => {
+            this.setState({
+                currentDate: new Date().toLocaleDateString()
+            })
+        }, 1000);
+    }
 
-const Clock = ()=> (
-<Container>
+    componentWillUnmount() {
+        clearInterval(this.getTime);
+        clearInterval(this.getDate);
+    }
 
-</Container>
-)
+    render() {
+        if (this.state.currentTime === null || this.state.currentDate === null) {
+            return (
+                <>
+                    Loading...
+                </>
+            )
+        }
+        return (
+            <>
+                <h2>{this.state.currentTime}</h2>
+                <h3>{this.state.currentDate}</h3>
+            </>
+        )
+    }
+}
